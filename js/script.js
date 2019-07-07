674a5ff0539f4bdbd54b1d538b2371845b3441a0
Krzysztof Branecki <krzysztof@branecki.net>
2019-07-07 23:19:12 +0200

    Add smooth scrolling efect to navigation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(".navbar .nav-link").click(function() {
+  var sectionTo = $(this).attr("href");
+  $("html, body").animate(
+    {
+      scrollTop: $(sectionTo).offset().top
+    },
+    1500
+  );
+});
